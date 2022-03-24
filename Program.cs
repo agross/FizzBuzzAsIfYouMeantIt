6c986e592b83951464695f9a523630e96e122260
Alexander Groß <agross@therightstuff.de>
2022-03-24 18:08:42 +0100

    Should_return_number_for_integer: red

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Xunit;
+
 Console.WriteLine("Hello, World!");
+
+public class FizzBuzzTests
+{
+  [Fact]
+  void Should_return_number_for_integer()
+  {
+    Assert.Equal("1", FizzBuzz(1));
+  }
+
+  string FizzBuzz(int number)
+  {
+    return "";
+  }
+}
